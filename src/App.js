bca36f78626d7c17f8376dda6577becd93ecc36b
Nikita <nburzhik@mail.ru>
2024-03-09 16:47:09 +0300

    Add first useState

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [state, setState] = useState(0);
+
+  const increament = () => setState(prev => prev +1);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Value = {state}</h1>
+        <button onClick={increament}>increament</button>
       </header>
     </div>
   );
